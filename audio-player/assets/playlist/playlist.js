c564e59ef1188af32a9353618f31ca38154ad8c1
Farid Kurbanou <kurbanou.faridun@gmail.com>
2025-05-07 10:24:34 +0300

    feat: audio-player add

---
+++ audio-player/assets/playlist/playlist.js
@@ -0,0 +1,62 @@
+let playlist = [
+    {
+        name: 'Winamp - Starts',
+        artist: 'Winamp',
+        img: 'track-0',
+        src: './assets/playlist/audio/track-0.mp3',
+    },
+    {
+        name: 'Far From Any Road',
+        artist: 'The Handsome Family',
+        img: 'track-1',
+        src: './assets/playlist/audio/track-1.mp3',
+    },
+    {
+        name: 'Hurt',
+        artist: 'Johnny Cash',
+        img: 'track-2',
+        src: './assets/playlist/audio/track-2.mp3',
+    },
+    {
+        name: 'Yeah Right',
+        artist: 'Joji',
+        img: 'track-3',
+        src: './assets/playlist/audio/track-3.mp3',
+    },
+    {
+        name: 'Gnossienne: No. 1',
+        artist: 'Erik Satie',
+        img: 'track-4',
+        src: './assets/playlist/audio/track-4.mp3',
+    },
+    {
+        name: 'Gnossienne: No. 2',
+        artist: 'Erik Satie',
+        img: 'track-5',
+        src: './assets/playlist/audio/track-5.mp3',
+    },
+    {
+        name: 'Gnossienne: No. 3',
+        artist: 'Erik Satie',
+        img: 'track-6',
+        src: './assets/playlist/audio/track-6.mp3',
+    },
+    {
+        name: 'Gnossienne: No. 4',
+        artist: 'Erik Satie',
+        img: 'track-7',
+        src: './assets/playlist/audio/track-7.mp3',
+    },
+    {
+        name: 'Gnossienne: No. 5',
+        artist: 'Erik Satie',
+        img: 'track-8',
+        src: './assets/playlist/audio/track-8.mp3',
+    },
+    {
+        name: 'Gnossienne: No. 6',
+        artist: 'Erik Satie',
+        img: 'track-9',
+        src: './assets/playlist/audio/track-9.mp3',
+    },
+];
